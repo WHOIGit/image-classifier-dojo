4af0228a01ecc1240723c60b5eac0522429f2fd2
Sidney <sbatchelder@whoi.edu>
2024-12-02 21:05:52 -0500

    restructuring

---
+++ src/multiclass/callbacks.py
@@ -0,0 +1,184 @@
+import os
+import json
+from typing import Optional, Literal
+
+import aim
+import plotly.graph_objects as go
+import h5py as h5
+import numpy as np
+from scipy.io import savemat
+import lightning.pytorch as pl
+from torchmetrics.functional.classification.confusion_matrix import _confusion_matrix_reduce
+
+
+class BarPlotMetricAim(pl.callbacks.Callback):
+    def __init__(self, metric, title='{METRIC} (ep{EPOCH})', best_only=True, order_by:str='classes', order_reverse=False):
+        self.metric_key = metric
+        self.title = title
+        self.best_only = best_only
+        self.order_by = order_by
+        self.order_reverse = order_reverse
+
+    def on_validation_end(self, trainer, pl_module):
+        epoch = pl_module.current_epoch
+        if not(pl_module.best_epoch==epoch or not self.best_only):
+            return
+
+        metrics = pl_module.metrics
+        metric_obj = metrics[self.metric_key]
+
+        classes = trainer.datamodule.classes
+        categories = [f'{c} {i:>03}' for i,c in enumerate(classes)]
+        scores = metric_obj.compute().cpu().numpy()
+        counts = trainer.datamodule.validation_dataset.count_perclass.values()
+
+        if self.order_by.lower() in ['class', 'classes', 'classlist']:
+            order_by = [i for i,c in enumerate(classes)]
+        elif self.order_by.lower() == 'alphabetical':
+            order_by = [c for i,c in enumerate(classes)]
+        elif 'count' in self.order_by:
+            order_by = counts
+        elif self.order_by == self.metric_key:
+            order_by = scores
+        else:
+            order_by = metrics[self.order_by].compute().cpu()
+
+        sorted_categories,sorted_scores,sorted_counts,sorted_orderby = zip(*sorted(zip(categories, scores, counts, order_by), key=lambda x: x[-1], reverse=self.order_reverse))
+        on_hover = [f' Score: {s}<br>Counts: {c}' for c,s in zip(sorted_counts,sorted_scores)]
+
+        # Create horizontal bar graph
+        fig = go.Figure(go.Bar(
+            y=sorted_categories,  # categories on y-axis
+            x=sorted_scores,  # values on x-axis
+            orientation='h',  # horizontal bars
+            text=[f'{score:.3f}' for score in sorted_scores],
+            hovertext=on_hover,
+            hoverinfo='text',
+        ))
+
+        # Calculate height based on the number of categories
+        title = self.title.format(METRIC=self.metric_key, EPOCH=epoch, ORDER=self.order_by)
+        height = 400 + len(categories) * 12  # base height + additional height per category
+
+        # Customize the layout
+        fig.update_layout(
+            title=title,
+            xaxis_title=self.metric_key,
+            yaxis_title='Classes',
+            height=height,     # Adjust height based on number of categories
+            font = dict(family='Courier New, monospace',size=10),
+            hoverlabel = dict(font_family="Courier New, monospace", font_size=12),
+        )
+
+        aim_figure = aim.Figure(fig)
+        #name, context = trainer.logger.parse_context(self.metric_key)
+        name = self.metric_key
+        context = dict(figure_order = self.order_by)
+        [logger.experiment.track(aim_figure, name=name, epoch=epoch, context=context) for logger in trainer.loggers]
+
+
+class PlotConfusionMetricAim(pl.callbacks.Callback):
+    def __init__(self, title='Confusion Matrix (ep{EPOCH})',
+                 normalize: Optional[Literal[True, "true", "pred", "all", "none", None]] = None,
+                 order_by:str='recall_perclass', best_only=True):
+        self.metric_key = 'confusion_matrix'
+        self.normalize = 'true' if normalize is True else normalize
+        self.title = title
+        self.best_only = best_only
+        self.order_by = order_by or 'classes'
+
+    def on_validation_end(self, trainer, pl_module):
+        epoch = pl_module.current_epoch
+        if not(pl_module.best_epoch==epoch or not self.best_only):
+            return
+
+        metrics = pl_module.metrics
+        metric_obj = metrics[self.metric_key]
+
+        classes = trainer.datamodule.classes
+        counts = trainer.datamodule.validation_dataset.count_perclass.values()
+        matrix_counts = metric_obj.compute().cpu()
+        categories = [f'({int(sum)}) {c} {i:>03}' for i, c, sum in zip(range(len(classes)), classes, matrix_counts.sum(dim=1))]
+        categories_x = [f'{i:>03} {c} ({int(sum)})' for i, c, sum in zip(range(len(classes)), classes, matrix_counts.sum(dim=0))]
+
+        order_reverse = False
+        if self.order_by.lower() in ['class', 'classes', 'classlist']:
+            order_by = [i for i,c in enumerate(classes)]
+        elif self.order_by.lower() == 'alphabetical':
+            order_by = [c for i,c in enumerate(classes)]
+        elif 'count' in self.order_by:
+            order_by = counts
+            order_reverse = True
+        else:
+            order_by = metrics[self.order_by].compute().cpu()
+            order_reverse = True
+
+        matrix = _confusion_matrix_reduce(matrix_counts, self.normalize)
+
+        ordered_classes, ordered_categories, ordered_categories_x, ordered_counts, ordered_orderby = \
+            zip(*sorted(zip(classes, categories, categories_x, counts, order_by),
+                        key=lambda x: x[-1], reverse=order_reverse))
+        reordered_indices = [categories.index(c) for c in ordered_categories]
+
+        ordered_matrix = matrix[reordered_indices, :][:, reordered_indices]
+        ordered_matrix_count = matrix_counts[reordered_indices, :][:, reordered_indices]
+
+        html_cells = []
+        recall = metrics['recall_perclass'].compute().cpu()
+        precision = metrics['precision_perclass'].compute().cpu()
+        f1score = metrics['f1_perclass'].compute().cpu()
+        for i, class_actual in enumerate(ordered_classes):
+            rows = []
+            for j, class_predicted in enumerate(ordered_classes):
+                lines = []
+                cell_count = int(ordered_matrix_count[i, j])
+                actuals_count = int(ordered_matrix_count.sum(dim=1)[i])
+                predicteds_count = int(ordered_matrix_count.sum(dim=0)[j])
+                lines.append(f'<b>    Count:</b> {cell_count}')
+                lines.append(f'<b>   Actual:</b> {classes.index(class_actual):>03} {class_actual}')
+                lines.append(f'<b>Predicted:</b> {classes.index(class_predicted):>03} {class_predicted}')
+                if i==j:
+                    class_idx = classes.index(class_actual)
+                    lines.append(' ---------------- ')
+                    lines.append(f'<b>   Recall:</b> {recall[class_idx]:.3f} ({cell_count} of {actuals_count})')
+                    lines.append(f'<b>Precision:</b> {precision[class_idx]:.3f} ({cell_count} of {predicteds_count})')
+                    lines.append(f'<b> F1 Score:</b> {f1score[class_idx]:.3f}')
+                # TODO show images in cells
+                #      or embed links
+                hovertext = '<BR>'.join(lines)  # + f'<extra></extra>'
+                rows.append(hovertext)
+            html_cells.append(rows)
+
+        # Create the heatmap plot using Plotly
+        fig = go.Figure(data=go.Heatmap(
+            z=ordered_matrix,
+            x=ordered_categories_x,
+            y=ordered_categories,
+            colorscale='Viridis',
+            colorbar=dict(title='Count'),
+            showscale=True,
+            hovertext=html_cells,
+            hoverinfo='text'
+        ))
+
+        # Calculate height based on the number of categories
+        title = self.title.format(METRIC=self.metric_key, EPOCH=epoch, ORDER=order_by)
+        height = 400 + len(categories) * 10  # base height + additional height per category
+
+        # Customize the layout
+        fig.update_layout(
+            title=title,
+            yaxis_title='Validation Actuals',
+            xaxis_title='Validation Predicteds',
+            height=height,
+            width=height,
+            font=dict(family='Courier New, monospace', size=10),
+            hoverlabel=dict(font_family="Courier New, monospace", font_size=12),
+            yaxis=dict(visible=True, autorange='reversed'),
+        )
+
+        aim_figure = aim.Figure(fig)
+        #name, context = trainer.logger.parse_context(self.metric_key)
+        name = self.metric_key
+        context = dict(figure_order = self.order_by, normalize=self.normalize)
+        [logger.experiment.track(aim_figure, name=name, epoch=epoch, context=context) for logger in trainer.loggers]
---
+++ src/multiclass/datasets.py
@@ -0,0 +1,144 @@
+import os
+import random
+
+import lightning as L
+import lightning.pytorch as pl
+from torch.utils.data import random_split, DataLoader
+from torch.utils.data.dataset import Dataset
+
+import torchvision
+from torchvision import transforms
+
+
+class ImageDatasetWithSource(Dataset):
+    """
+    Custom dataset that includes source file paths in addition to data and target. Also checks for valid image extention.
+    Example setup:     dataloader = torch.utils.DataLoader(ImageDatasetWithPaths(list_of_image_paths))
+    Example usage:     for inputs,labels,source in my_dataloader: ....
+    instead of:        for inputs,labels in my_dataloader: ....
+    adapted from: https://gist.github.com/andrewjong/6b02ff237533b3b2c554701fb53d5c4d
+    """
+    def __init__(self, sources_targets, classes, transform=None):
+        self.sources, self.targets = zip(*sources_targets)
+        self.classes = classes
+        self.transform = transform
+
+    @property
+    def labels(self):
+        return [self.classes[idx] for idx in self.targets]
+
+    def __getitem__(self, index):
+        src = self.sources[index]
+        target = self.targets[index]
+        sample = torchvision.datasets.folder.default_loader(src)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        return sample, target, src
+
+    def __len__(self):
+        return len(self.targets)
+
+    @property
+    def images_perclass(self):
+        ipc = {c:[] for c in self.classes}
+        for src_img, target in zip(self.sources, self.targets):
+            ipc[self.classes[target]].append(src_img)
+        return ipc
+
+    @property
+    def count_perclass(self):
+        cpc = {c:0 for c in self.classes} # initialize list at 0-counts
+        for class_label in self.labels:
+            cpc[class_label] += 1
+        return cpc
+
+
+class ImageListsWithLabelIndex(L.LightningDataModule):
+    def __init__(self, train_src, val_src, classlist, base_transforms, training_transforms=[], test_src=None, batch_size=108, num_workers=4):
+        super().__init__()
+        self.training_source = train_src
+        self.validation_source = val_src
+        self.test_source = test_src
+        self.classes = self.parse_classlist(classlist)
+        self.base_transforms = base_transforms
+        self.training_transforms = training_transforms
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.training_dataset = None
+        self.validation_dataset = None
+        self.test_dataset = None
+
+        assert len(set(self.classes))==len(self.classes), 'Class list has duplicate labels!'
+
+    @staticmethod
+    def parse_classlist(classlist):
+        with open(classlist) as f:
+            return f.read().splitlines()
+
+    def parse_listfile_with_targets(self, listfile):
+        num_classes = len(self.classes)
+        bad_class = []
+        bad_file = []
+        bad_ext = []
+        sources_targets = []
+        with open(listfile) as f:
+            for line in f.read().splitlines():
+                if not line: continue
+                trg, src = line.split()
+                trg = int(trg)
+                if not trg < num_classes:
+                    print(f'BAD TRG: trg > num_classes {trg} < {num_classes} for {src}')
+                    bad_class.append(line)
+                elif not torchvision.datasets.folder.is_image_file(src):
+                    print(f'BAD EXT: {src}')
+                    bad_ext.append(line)
+                elif not os.path.isfile(src):
+                    print(f'BAD FILE: {src}')
+                    bad_file.append(line)
+                else:
+                    sources_targets.append((src, trg))
+        errors = bad_file + bad_ext + bad_class
+        return sources_targets, errors
+
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit":
+            training_samples, train_ds_errors = self.parse_listfile_with_targets(self.training_source)
+            validation_samples, val_ds_errors = self.parse_listfile_with_targets(self.validation_source)
+
+            if train_ds_errors or val_ds_errors:
+                print('BAD SAMPLES:', len(train_ds_errors)+len(val_ds_errors))
+                raise RuntimeError
+
+            validation_transform = transforms.Compose(self.base_transforms)
+            training_transform = transforms.Compose(self.training_transforms + self.base_transforms)
+
+            self.training_dataset = ImageDatasetWithSource(sources_targets=training_samples, classes=self.classes, transform=training_transform)
+            self.validation_dataset = ImageDatasetWithSource(sources_targets=validation_samples, classes=self.classes, transform=validation_transform)
+
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test":
+            self.testing_dataset = ...
+
+
+    def train_dataloader(self):
+        return DataLoader(self.training_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.validation_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.testing_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    @property
+    def count_perclass(self, datasets='fit'):
+        if datasets=='fit':
+            ipc = {c: set() for c in self.classes}
+            for src_img, target in zip(self.training_dataset.sources+self.validation_dataset.sources,
+                                       self.training_dataset.targets+self.validation_dataset.targets):
+                ipc[self.classes[target]].add(src_img)
+            cpc = {cls:len(src_imgs) for cls,src_imgs in ipc.items()}
+            return cpc
+
